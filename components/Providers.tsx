5580945ffca114dc8fa11aa43f3babc618bbea68
favolia <daisszo1455@gmail.com>
2024-09-15 19:04:16 +0800

    remove @tanstack/react-query

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,12 +1,6 @@
 'use client';
 import { ThemeProvider, useTheme } from 'next-themes';
 import { Toaster } from 'sonner';
-// import {
-//     QueryClient,
-//     QueryClientProvider,
-// } from '@tanstack/react-query'
-
-// const queryClient = new QueryClient()
 
 const ToasterProvider = () => {
     const { systemTheme } = useTheme()
@@ -16,10 +10,8 @@ const ToasterProvider = () => {
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
         <ThemeProvider attribute="class" enableSystem disableTransitionOnChange defaultTheme="system">
-            {/* <QueryClientProvider client={queryClient}> */}
             <ToasterProvider />
             {children}
-            {/* </QueryClientProvider> */}
         </ThemeProvider>
     )
 }
