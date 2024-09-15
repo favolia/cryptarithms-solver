74b3b3f03b4e73aedea464a2f38dc58fe22f5c7c
favolia <daisszo1455@gmail.com>
2024-09-15 18:54:18 +0800

    remove file

--- utils/getSlotIndex.tsx
+++
@@ -1,7 +0,0 @@
-// const getSlotIndex = (rowIndex, colIndex) => {
-//     let index = 0;
-//     for (let i = 0; i < rowIndex; i++) {
-//         index += slots.rowWords[i].length;
-//     }
-//     return index + colIndex;
-// };
